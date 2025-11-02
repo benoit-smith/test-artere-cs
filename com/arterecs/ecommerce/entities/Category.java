4c61104a44b1f60f8d9f1fd6a2624c83ca307e95
Benoît Smith <62805693+benoit-smith@users.noreply.github.com>
2025-11-02 17:13:54 +0100

    Create Category class with properties and methods

---
+++ com/arterecs/ecommerce/entities/Category.java
@@ -0,0 +1,50 @@
+package com.arterecs.ecommerce.entities;
+
+public class Category {
+
+    private Long categoryId;
+    private String name;
+    private String description;
+    private Long parentCategoryId;
+
+    public CategoryId() {
+        super();
+    }
+
+    public CategoryId(Long categoryId, String name, String description, Long parentCategoryId) {
+        super();
+        this.categoryId = categoryId;
+        this.name = name;
+        this.description = description;
+        this.parentCategoryId = parentCategoryId;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getParentCategoryId() {
+        return parentCategoryId;
+    }
+
+    public void setParentCategoryId(Long parentCategoryId) {
+        this.parentCategoryId = parentCategoryId;
+    }
+
+}
