a4cac6a15abd8b17b8b831d9bab49edab9917948
Benoît Smith <62805693+benoit-smith@users.noreply.github.com>
2025-11-02 18:10:45 +0100

    Implement cart retrieval and total cost calculation
    Added methods to retrieve cart and calculate total cost.

--- com/arterecs/ecommerce/services/CartService.java
+++ com/arterecs/ecommerce/services/CartService.java
@@ -1,6 +1,36 @@
 package com.arterecs.ecommerce.services;
 
 import com.arterecs.ecommerce.entities.*;
+import java.util.List;
+import org.apache.commons.collections4.CollectionUtils;
 
 public class CartService {
+
+    private ProductService productService;
+
+    public CartService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public getCart(Long cartId) {
+        return null; //TODO retrieve Cart entity by identifier cartId
+    }
+
+    public List<Cart> getCustomerCart(Long customerId) {
+        return null; //TODO retrieve all Cart entities by customerId
+    }
+
+    public Long getCartTotalCost(Long customerId) {
+        List<Cart> customerCart = this.getCustomerCart(Long customerId);
+        if (CollectionUtils.isEmpty(customerCart)) {
+            return 0d;
+        }
+        return customerCart.stream()
+            .map(cart -> {
+                Product product = productService.getProduct(cart.getProductId());
+                return (Double) (cart.getCount() * product.getPrice());
+            })
+            .reduce(0, Double::sum);
+    }
+
 }
